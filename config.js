c76a7081c9b6194f28fc8edceee435710776a9de
cazuba <krlos2290@gmail.com>
2018-06-13 17:35:22 -0600

    Adding all env variables one by one

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
-let envVariables = process.env;
+let envVariables = {};
 if (process.env.NODE_ENV !== 'test') {
   const process = require('./process.json');
   envVariables = process.apps[0].env;
 } else {
+  envVariables.NODE_ENV = process.env.NODE_ENV;
+  envVariables.SUCCESS_MESSAGE = process.env.SUCCESS_MESSAGE;
   envVariables.SUCCESS_JSON = JSON.parse(process.env.SUCCESS_JSON);
+  envVariables.PORT = process.env.PORT;
 }
 
 const config = {
