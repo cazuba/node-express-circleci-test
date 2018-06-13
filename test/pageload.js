58bf48149dc04decd2739ce5d37f22b6aa9779f0
cazuba <krlos2290@gmail.com>
2018-06-12 21:22:46 -0600

    Test

--- test/pageload.js
+++ test/pageload.js
@@ -1,7 +1,8 @@
 // Require the dev-dependencies
 const chai = require('chai');
-
 const chaiHttp = require('chai-http');
+
+const server = require('../server');
 
 chai.use(chaiHttp);
 
@@ -11,18 +12,17 @@ chai.use(chaiHttp);
 describe('GET /holamundo', () => {
   it('should return hola mundo', done => {
     const creds = {
-      email: 'thewing@thehangar.cr',
-      password: 'wingtesting!2018'
+      email: '',
+      password: ''
     };
     chai
       .request(server)
-      .post('/api/auth/login')
+      .post('/holamundo')
       .send(creds)
       .end((err, res) => {
         res.should.have.status(200);
         res.body.should.be.a('object');
-        res.body.should.have.property('auth').eql(true);
-        res.body.should.have.property('token');
+        res.body.should.have.property('success').eql(true);
         done();
       });
   });
