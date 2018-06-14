8f3d086b15f5a81741121d75e4831a097e83c492
cazuba <krlos2290@gmail.com>
2018-06-13 18:13:22 -0600

    CircleCI workflows working

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const config = require('./config.js');
 const app = express();
 
 app.get('/', (req, res) => res.status(200).send(config.SUCCESS_MESSAGE));
-
 app.get('/holamundo', (req, res) => res.status(200).send(config.SUCCESS_JSON));
 
 module.exports = app;
